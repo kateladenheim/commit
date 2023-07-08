5b23f38c150d5fe3923c2cac6761fc52e53ec462
Kate Ladenheim <ladenheim.k@gmail.com>
2023-07-08 14:50:31 -0700

    Update for Unity integration
    updated server.js and installed websocket

--- server.js
+++ server.js
@@ -5,10 +5,35 @@ var server = require('socket.io')(http);
 var path = require('path');
 var port = process.env.PORT || 3000; // Use the Heroku environment variable for the port, or fallback to a default value (e.g., 3000)
 
+var WebSocket = require('websocket').server;
+var unitySocket;
+
 var counter1 = 0; // Initial counter value for button 1
 var counter2 = 0; // Initial counter value for button 2
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Create a WebSocket server
+var wsServer = new WebSocket({
+    httpServer: http,
+    autoAcceptConnections: false
+});
+
+wsServer.on('request', function(request) {
+    console.log('Unity connected');
+    var connection = request.accept(null, request.origin);
+    unitySocket = connection;
+
+    connection.on('message', function(message) {
+        // Handle any incoming messages from Unity if needed
+        console.log('Received message from Unity:', message);
+    });
+
+    connection.on('close', function(reasonCode, description) {
+        console.log('Unity disconnected');
+        unitySocket = null;
+    });
+});
 
 server.on('connection', function(socket) {
     console.log('A user connected');
@@ -21,12 +46,18 @@ server.on('connection', function(socket) {
     socket.on('clicked1', function() {
         counter1 += 1; // Increment click count for button 1
         server.emit('click_count1', counter1); // Send new counter value to all users
+        if (unitySocket && unitySocket.connected) {
+            unitySocket.send(JSON.stringify({ button: 1, count: counter1 }));
+        }
     });
 
     // When user clicks button 2
     socket.on('clicked2', function() {
         counter2 += 1; // Increment click count for button 2
         server.emit('click_count2', counter2); // Send new counter value to all users
+        if (unitySocket && unitySocket.connected) {
+            unitySocket.send(JSON.stringify({ button: 2, count: counter2 }));
+        }
     });
 });
 
