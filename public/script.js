d1441011b871dd0d1426d86ab304bc9a8d932ebd
Kate Ladenheim <ladenheim.k@gmail.com>
2023-08-24 11:43:49 -0400

    Update script.js
    remove redirect entirely to see whats up

--- public/script.js
+++ public/script.js
@@ -19,17 +19,17 @@ $(function () {
     // Emit click event for button 1
 $('#btn_click1').click(function() {
     socket.emit('clicked1'); 
-    setTimeout(function() {
-        window.location.href = 'https://www.kateladenheim.com/commit-yes';
-    }, 1000);  // Delaying the redirection by 1 second
+    // setTimeout(function() {
+    //     window.location.href = 'https://www.kateladenheim.com/commit-yes';
+    // }, 1000);  // Delaying the redirection by 1 second
 });
 
     // Emit click event for button 2
     $('#btn_click2').click(function() {
     socket.emit('clicked2');
-    setTimeout(function() {
-        window.location.href = 'https://www.kateladenheim.com/commit-no';
-    }, 1000);  // Delaying the redirection by 1 second
+    // setTimeout(function() {
+    //     window.location.href = 'https://www.kateladenheim.com/commit-no';
+    // }, 1000);  // Delaying the redirection by 1 second
 });
 
 });
