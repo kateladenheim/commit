615c8d36e8bde16faeafd726e1bc2a533f161908
Kate Ladenheim <ladenheim.k@gmail.com>
2023-08-25 17:50:50 -0400

    Update script.js
    testing to see if the refresh works...

--- public/script.js
+++ public/script.js
@@ -19,18 +19,18 @@ $(function () {
     // Emit click event for button 1
     $('#btn_click1').click(function() {
         socket.emit('clicked1', function(acknowledged) {
-            // if (acknowledged) {
-            //     window.location.href = 'https://www.kateladenheim.com/commit-yes';
-            // }
+            if (acknowledged) {
+                window.location.href = 'https://www.kateladenheim.com/commit-yes';
+            }
         });
     });
 
     // Emit click event for button 2
     $('#btn_click2').click(function() {
         socket.emit('clicked2', function(acknowledged) {
-            // if (acknowledged) {
-            //     window.location.href = 'https://www.kateladenheim.com/commit-no';
-            // }
+            if (acknowledged) {
+                window.location.href = 'https://www.kateladenheim.com/commit-no';
+            }
         });
     });
 
