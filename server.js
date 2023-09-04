18280e5652783bc9c6e7c60c1651894efae18dda
Kate Ladenheim <ladenheim.k@gmail.com>
2023-09-04 14:07:46 -0400

    trying OSC
    trying OSC

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ var http = require('http').Server(app);
 var server = require('socket.io')(http);
 var path = require('path');
 var port = process.env.PORT || 3000; // Use the Heroku environment variable for the port, or fallback to a default value (e.g., 3000)
-var WebSocket = require('websocket').server;
 
 const { Pool } = require('pg');
+var osc = require('node-osc');
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL, // This will be set by Heroku once you've provisioned a Postgres add-on
     ssl: {
@@ -14,7 +15,8 @@ const pool = new Pool({
     }
 });
 
-var unitySocket;
+// Initialize OSC client
+var oscClient = new osc.Client('127.0.0.1', 8000);  // The IP and Port of your Unity App listening for OSC messages
 
 var counter1 = 0; // Initial counter value for button 1
 var counter2 = 0; // Initial counter value for button 2
@@ -36,28 +38,6 @@ pool.query('SELECT button1_count, button2_count FROM click_counts WHERE id = 1',
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Create a WebSocket server
-var wsServer = new WebSocket({
-    httpServer: http,
-    autoAcceptConnections: false
-});
-
-wsServer.on('request', function(request) {
-    console.log('Unity connected');
-    var connection = request.accept(null, request.origin);
-    unitySocket = connection;
-
-    connection.on('message', function(message) {
-        // Handle any incoming messages from Unity if needed
-        console.log('Received message from Unity:', message);
-    });
-
-    connection.on('close', function(reasonCode, description) {
-        console.log('Unity disconnected');
-        unitySocket = null;
-    });
-});
-
 server.on('connection', function(socket) {
     console.log('A user connected');
 
@@ -70,9 +50,10 @@ server.on('connection', function(socket) {
         counter1 += 1; 
         pool.query('UPDATE click_counts SET button1_count = $1 WHERE id = 1', [counter1]); // Update the database
         server.emit('click_count1', counter1);
-        if (unitySocket && unitySocket.connected) {
-            unitySocket.send(JSON.stringify({ button: 1, count: counter1 }));
-        }
+
+        // Send an OSC message for button 1
+        oscClient.send('/clickCount1', counter1);
+
         ack(true);  // Acknowledge that the click has been processed
     });
 
@@ -81,10 +62,11 @@ server.on('connection', function(socket) {
         counter2 += 1; // Increment click count for button 2
         pool.query('UPDATE click_counts SET button2_count = $1 WHERE id = 1', [counter2]); // Update the database
         console.log(counter2);
-        server.emit('click_count2', counter2); // Send new counter value to all users
-        if (unitySocket && unitySocket.connected) {
-            unitySocket.send(JSON.stringify({ button: 2, count: counter2 }));
-        }
+        server.emit('click_count2', counter2);
+
+        // Send an OSC message for button 2
+        oscClient.send('/clickCount2', counter2);
+
         ack(true);
     });
 });
@@ -93,11 +75,3 @@ server.on('connection', function(socket) {
 http.listen(port, function() {
     console.log('Listening on port: ' + port);
 });
-
-// app.get('/handle-reload', function(req, res) {
-//     if (unitySocket && unitySocket.connected) {
-//         unitySocket.close();
-//         console.log('WebSocket closed due to page reload');
-//     }
-//     res.status(200).send('Handled reload');
-// });
